86387e2c1c1502fdb198ab8d56dda50e5dcc2b9b
Rosendo Garcia <grosendoh73@gmail.com>
2023-09-16 15:40:36 -0600

    make first steps

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,14 @@ import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('app'))
 root.render(
-  <h3>Hola mundillo</h3>
+  <>
+    <div className='container'>
+      <h3>React App</h3>
+      <div className='content'>
+        <h1>React App</h1>
+        <p>React App</p>
+        <p>React App 2</p>
+      </div>
+    </div>
+  </>
 )
