840da036caa1de18e9ec1931422d8dc3b5196cfa
Rosendo Garcia <grosendoh73@gmail.com>
2023-09-25 11:23:05 -0600

    update email

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -15,7 +15,7 @@ export const PersonalInfo = () => {
         <p className='myName'>Rosendo Garcia</p>
       </section>
       <section className='piSocials'>
-        <InfoItem icon={Email} text='grosendoh73@gmail.com' url='grosendoh73@gmail.com' mailto />
+        <InfoItem icon={Email} text='rosendo.garcia73@outlook.com' url='rosendo.garcia73@outlook.com' mailto />
         <InfoItem icon={GitHub} text='@Roosendo' url='https://github.com/Roosendo' mailto={false} />
         <InfoItem icon={Linkedin} text='Rosendo Garcia' url='https://www.linkedin.com/in/rosendo-garcia-a498b9279/' mailto={false} />
         <InfoItem icon={Instagram} text='rosendonbhd' url='https://www.instagram.com/rosendonbhd/' mailto={false} />
