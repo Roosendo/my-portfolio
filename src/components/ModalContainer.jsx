2d6fc6b43733931fa51eb6475e9852b7ddc2bf9c
Rosendo Garcia <grosendoh73@gmail.com>
2023-12-21 17:51:57 -0600

    changes to the projects, it is not finished yet

--- src/components/ModalContainer.jsx
+++ src/components/ModalContainer.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
-import Modal from './Modal.jsx'
+import Modal from 'react-modal'
+import '../assets/stylesheets/Modal.css'
 
 const ModalContainer = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
 
-  function openModal () {
-    setIsOpen(true)
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false)
   }
 
   return (
     <>
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className='modal'
+        ariaHideApp={false}
+        style={{
+          overlay: { backgroundColor: 'rgba(100, 100, 100, 0.75)' }
+        }}
+      >
         {children}
+        <button onClick={closeModal}>Close</button>
       </Modal>
       <button onClick={openModal}>View Projects</button>
     </>
