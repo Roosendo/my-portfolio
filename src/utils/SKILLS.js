e5c781b576c8e4b140fa39c898be8156371151b8
Rosendo Garcia <grosendoh73@gmail.com>
2024-01-07 21:05:12 -0600

    Refactor component props and remove unused variables

---
+++ src/utils/SKILLS.js
@@ -0,0 +1,48 @@
+import AstroIcon from '../assets/icons/skills/AstroIcon.astro'
+import CSS from '../assets/icons/skills/CSS.astro'
+import Express from '../assets/icons/skills/Express.astro'
+import HTML from '../assets/icons/skills/HTML.astro'
+import JavaScript from '../assets/icons/skills/JavaScript.astro'
+import Node from '../assets/icons/skills/Node.astro'
+import React from '../assets/icons/skills/React.astro'
+import Tailwind from '../assets/icons/skills/Tailwind.astro'
+import TypeScript from '../assets/icons/skills/TypeScript.astro'
+
+export const SKILLS = [
+  {
+    name: 'HTML',
+    icon: HTML
+  },
+  {
+    name: 'CSS',
+    icon: CSS
+  },
+  {
+    name: 'JavaScript',
+    icon: JavaScript
+  },
+  {
+    name: 'Node',
+    icon: Node
+  },
+  {
+    name: 'TypeScript',
+    icon: TypeScript
+  },
+  {
+    name: 'Express',
+    icon: Express
+  },
+  {
+    name: 'Tailwind',
+    icon: Tailwind
+  },
+  {
+    name: 'React',
+    icon: React
+  },
+  {
+    name: 'Astro',
+    icon: AstroIcon
+  }
+]
