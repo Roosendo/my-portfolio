0dec54c68520b8ec9264dd3723cf5704d019c830
Rosendo Garcia <grosendoh73@gmail.com>
2024-01-21 14:25:34 -0600

    Refactor showAndHideAlert function

--- src/utils/CopyFunctionality.ts
+++ src/utils/CopyFunctionality.ts
@@ -1,3 +1,12 @@
+export function showAndHideAlert (alertElement: HTMLElement, timeout: number = 2000) {
+  alertElement.classList.remove('hidden')
+  alertElement.classList.add('flex')
+  setTimeout(() => {
+    alertElement.classList.remove('flex')
+    alertElement.classList.add('hidden')
+  }, timeout)
+}
+
 export function copyToClipboard(text: string): boolean {
   const tempInput = document.createElement('textarea')
   tempInput.value = text
--- src/utils/ShowAndHideAlert.ts
+++
@@ -1,6 +0,0 @@
-export function showAndHideAlert (alertElement: HTMLElement, timeout: number = 2000) {
-  alertElement.classList.remove('hidden')
-  setTimeout(() => {
-    alertElement.classList.add('hidden')
-  }, timeout)
-}
