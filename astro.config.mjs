29d56b816139385a9180eb3f70ba1a3843d3ba36
Rosendo <grosendoh73@gmail.com>
2024-09-10 18:19:30 -0600

    ➕ Add SolidJs instead of React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
-import react from '@astrojs/react'
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), solidJs()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
