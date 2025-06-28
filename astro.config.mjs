89afae6d0891202368dafe9beb4f87e47995be31
Rosendo Garcia <grosendoh73@gmail.com>
2025-06-27 21:58:05 -0600

    🚨 Remove ESLint and Prettier configurations; add Biome configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
-import { defineConfig } from 'astro/config'
-import tailwindcss from '@tailwindcss/vite'
-import vercel from '@astrojs/vercel'
-import solidJs from '@astrojs/solid-js'
+import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
+    defaultLocale: "en",
+    locales: ["en", "es"],
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
 
-  output: 'static',
+  output: "static",
   adapter: vercel(),
 
   vite: {
-    plugins: [tailwindcss()]
-  }
-})
+    plugins: [tailwindcss()],
+  },
+});
